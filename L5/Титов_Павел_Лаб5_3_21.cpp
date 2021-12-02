99e4d96c69e8d1e14c8c2c0b282a36b322efde9a
ZetoOfficial <tpptgs@bk.ru>
2021-12-02 15:51:58 +0500

    add L8&&L10

--- L5/Титов_Павел_Лаб5_3_21.cpp
+++ L5/Титов_Павел_Лаб5_3_21.cpp
@@ -10,8 +10,10 @@ using namespace std;
 int main(){
     double num;
     cin >> num;
+    num += 0.000000000000001;
     int three = int(num * 1000) % 10;
     int two = int(num * 1000) / 10 % 10;
     int one = int(num * 1000) / 10 / 10 % 10;
+    cout << one << " " << two << " " << three << "\n";
     cout << (one % 2 == two % 2 && one % 2 == three % 2);
 }
---
+++ L8/Титов_Павел_Лаб8_1_21.cpp
@@ -0,0 +1,35 @@
+/*
+Титов Павел Сергеевич 04-1
+Лабораторная работа №8
+Задание 1 Вариант 21
+n= …
+int x [n];
+Вычислить для вектора x: 
+6. x[0]*1! + x[1]*2! + ... + x[n-1]*n! 
+*/
+#include <iostream>
+#include <vector>
+
+long double fact(int);
+
+using namespace std;
+int main() {
+    const int n = 5; 
+    vector<int> x(n);
+    for (int i = 0; i < n; i++){ 
+        x[i] = rand() % 10;
+    }
+
+    double s = 0;
+    for (int i = 1; i < n; i++){
+        s += x[i-1]*fact(i);
+    }
+    cout << s;
+}
+
+long double fact(int N)
+{
+    if(N < 0) return 0;
+    if (N == 0) return 1;
+    else return N * fact(N - 1);
+}
